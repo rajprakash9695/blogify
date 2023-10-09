3f1bcbe7bccf7c9dda1e0127c46cabc4976f9b38
Raj Prakash Maurya <raj.maurya@bksgold.com>
2023-10-09 13:34:39 +0530

    custom model created

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,20 @@ import "./App.css";
 import { Box } from "@mui/material";
 import Navbar from "./components/Navbar";
 import LoginForm from "./components/LoginForm";
+import CustomModal from "./components/CustomModel";
+import { useState } from "react";
 
 function App() {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   const handleLogin = (email: string, password: string) => {
     // Implement your login logic here
     console.log("Login:", email, password);
@@ -16,6 +28,16 @@ function App() {
   return (
     <Box>
       <Navbar />
+      <button onClick={handleOpen}>Open Modal</button>
+
+      <CustomModal
+        open={open}
+        onClose={handleClose}
+        title="Custom Modal"
+        maxWidth={"sm"}
+      >
+        This is the content of the custom modal.
+      </CustomModal>
       <LoginForm onLogin={handleLogin} onSignup={handleSignup} />
     </Box>
   );
