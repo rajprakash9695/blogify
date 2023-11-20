ebf9ab29b7f23453158ca8350ddfc55b0924a4a0
Raj Prakash Maurya <raj.maurya@bksgold.com>
2023-11-20 19:55:19 +0530

    ..

--- src/components/HomeBlog.tsx
+++ src/components/HomeBlog.tsx
@@ -45,7 +45,7 @@ function HomeBlog() {
       <CustomCard
         title="first"
         description="lohjkasdgfsfgbskjbskbdkjb"
-        image="https://images.unsplash.com/photo-1542435503-956c469947f6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YmxvZ3xlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80"
+        image="https://images.unsplash.com/photo-1542435503-c469947f6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YmxvZ3xlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80"
       />
       <CustomCard
         title="Typew  riter"
